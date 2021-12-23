2eeba8d379fa1bf9d5036e98caa3cf7bd94be2d0
awesomelewis2007 <awesomelewis2007@gmail.com>
2021-12-23 09:53:04 +0000

    Fix Spelling

--- Xmake/Global.cpp
+++ Xmake/Global.cpp
@@ -1,8 +1,8 @@
 //Xmake - By awesomelewis2007
 //Github:https://github.com/awesomelewis2007/Xmake
 //Xmake Global file
-// - Only put varibles for buid and other scripts 
-// - Do not put varible names that contain names from other hedders or scripts
+// - Only put variables for build and other scripts 
+// - Do not put variable names that contain names from other headers or scripts
 #include "Global.h"
 #include <fstream>
 #include <string>
